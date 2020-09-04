2be10fbad5cea42636c56cc3130a95b81dd8c8da
Scott Young <syoung@instructure.com>
2020-09-04 10:57:41 -0600

    form without styling

--- src/components/IpInput.js
+++ src/components/IpInput.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from 'axios';
+import * as Icons from "@fortawesome/fontawesome-free-solid";
+import axios from "axios";
+import { Button, Container, InputGroup } from "react-bootstrap";
 
 import AddressInfo from "./AddressInfo";
-
+import Styles from "../styles/Styles";
 
 class IpInput extends Component {
   constructor(props) {
     super(props);
-    console.log(this.props)
-    this.state = { 
-        value: "",
-        addrInfo: "" 
+    console.log(this.props);
+    this.state = {
+      value: "",
+      addrInfo: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -27,39 +29,54 @@ class IpInput extends Component {
   }
 
   getAddrInfo = (address) => {
-    axios.get(
-      `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_IPIFY_KEY}&ipAddress=${address}`
-    ).then(res => {
-        const location = res.data.location
-        this.setState({addrInfo: res.data});
+    axios
+      .get(
+        `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_IPIFY_KEY}&ipAddress=${address}`
+      )
+      .then((res) => {
+        const location = res.data.location;
+        this.setState({ addrInfo: res.data });
+        // sets location state for banner.js
         this.setLocation(location);
-    }).catch(err => {
-        console.log(err)
-    })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   setLocation = (location) => {
-    this.props.setLocation(location)
-  }
+    this.props.setLocation(location);
+  };
 
   render() {
     return (
-    <div>
-      <div>
+      <Container fluid>
         <form onSubmit={this.handleSubmit}>
           <label>
-            IP Address:
             <input
               type="text"
               value={this.state.value}
+              placeholder="Search for any IP address or domain"
               onChange={this.handleChange}
             />
           </label>
-          <input type="submit" value="submit" onClick={() => this.getAddrInfo(this.state.value)}/>
-          <AddressInfo ipAddr={this.state.addrInfo.ip} location={this.state.addrInfo.location} isp={this.state.addrInfo.isp} />
+
+          <Button
+            variant="dark"
+            style={Styles.inputButton}
+            type="submit"
+            value="submit"
+            onClick={() => this.getAddrInfo(this.state.value)}
+          >
+            <FontAwesomeIcon icon={Icons.faAngleRight} />
+          </Button>
+          <AddressInfo
+            ipAddr={this.state.addrInfo.ip}
+            location={this.state.addrInfo.location}
+            isp={this.state.addrInfo.isp}
+          />
         </form>
-      </div>
-    </div>
+      </Container>
     );
   }
 }
